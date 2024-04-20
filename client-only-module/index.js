5d4f5f725d6ade73c3b8d39f6cf07e513d8bfe6a
Devesh Kumar <devesh2027@gmail.com>
2024-04-20 16:08:57 +0530

    client-only-module: Update doc and error message

--- client-only-module/index.js
+++ client-only-module/index.js
@@ -1,6 +1,7 @@
 if (typeof window === "undefined") {
+	// In case this file does creep into any imports from a server module, throw an error and block dev/builds.
 	throw new Error(
-		"This module can only be used in a client environment." +
+		"This module can only be used in a client environment. " +
 			"It has been imported in a server environment."
 	);
 }
